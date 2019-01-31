424b47f8a5d535b5b5694d40d1a45e0031f45789
James Ogila <james@jamess-mbp>
2019-01-31 15:39:20 +0000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by James Ogila on 29/01/2019.
+//  Copyright © 2019 James Ogila. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
